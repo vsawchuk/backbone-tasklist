f35e88a3f6fd2e0a33b25f26275eb69bc50edb25
dee <dee@adadevelopersacademy.org>
2017-12-09 23:11:47 -0800

    Restore toggle complete functionality by adding toggle-complete event listening and handling to view. Give view listener to any change the model makes.

--- src/views/task_view.js
+++ src/views/task_view.js
@@ -5,6 +5,8 @@ import Task from '../models/task';
 const TaskView = Backbone.View.extend({
   initialize(params) {
     this.template = params.template;
+
+    this.listenTo(this.model, "change", this.render);
   },
   render() {
     const compiledTemplate = this.template(this.model.toJSON());
@@ -12,11 +14,16 @@ const TaskView = Backbone.View.extend({
     return this;
   },
   events: {
-    'click button.delete': 'deleteTask'
+    'click button.delete': 'deleteTask',
+    'click button.toggle-complete': 'toggleComplete',
   },
   deleteTask: function(e) {
     this.model.destroy();
     this.remove();
+  },
+  toggleComplete: function(e) {
+    this.model.set('is_complete', !this.model.get('is_complete'));
+    this.$el.closest('.task').toggleClass('is-complete');
   },
 });
 
