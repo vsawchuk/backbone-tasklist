ff955deb9b4ba60947fa53ea73601081d23c2bf3
dee <dee@adadevelopersacademy.org>
2017-12-09 23:58:33 -0800

    add adding a new task functionality

--- src/views/task_list_view.js
+++ src/views/task_list_view.js
@@ -1,5 +1,7 @@
 import Backbone from 'backbone';
+import _ from 'underscore';
 import TaskView from '../views/task_view';
+import Task from '../models/task'
 
 const TaskListView = Backbone.View.extend({
   initialize(params) {
@@ -23,6 +25,43 @@ const TaskListView = Backbone.View.extend({
       this.$('#todo-items').append(taskView.render().$el);
     });
     return this;
+  },
+  events: {
+    'click #add-new-task': 'addTask'
+  },
+  addTask: function(event) {
+    event.preventDefault();
+    const taskData = {};
+    ['task_name', 'assignee'].forEach( (field) => {
+      const val = this.$(`#add-task-form input[name=${field}]`).val();
+      if (val != '') {
+        taskData[field] = val;
+      }
+    });
+    const newTask = new Task(taskData);
+
+    if (newTask.isValid()) {
+      this.model.add(newTask);
+      this.updateStatusMessageWith(`New task added: ${newTask.get('task_name')}`);
+    } else {
+      this.updateStatusMessageFrom(newTask.validationError);
+    }
+  },
+  updateStatusMessageFrom: function(messageHash) {
+    const statusMessagesEl = this.$('#status-messages');
+    statusMessagesEl.empty();
+    _.each(messageHash, (messageType) => {
+      messageType.forEach((message) => {
+        statusMessagesEl.append(`<li>${message}</li>`);
+      })
+    });
+    statusMessagesEl.show();
+  },
+  updateStatusMessageWith: function(message) {
+    const statusMessagesEl = this.$('#status-messages');
+    statusMessagesEl.empty();
+    statusMessagesEl.append(`<li>${message}</li>`);
+    statusMessagesEl.show();
   }
 });
 
