934bc1c9728f04fcdf0032185dff617907f21a46
dee <dee@adadevelopersacademy.org>
2017-12-09 23:59:33 -0800

    clean up and remove unused add task code from app.js

--- src/app.js
+++ src/app.js
@@ -13,47 +13,7 @@ import TaskListView from './views/task_list_view';
 
 const taskList = new TaskList();
 
-// helper method for updating the DOM with the status from a hash
-const updateStatusMessageFrom = (messageHash) => {
-  $('#status-messages').empty();
-  _.each(messageHash, (messageType) => {
-    messageType.forEach((message) => {
-      $('#status-messages').append($(`<li>${message}</li>`));
-    })
-  });
-  $('#status-messages').show();
-}
-
-// helper method for updating the DOM with the status from a string
-const updateStatusMessageWith = (message) => {
-  $('#status-messages').empty();
-  $('#status-messages').append($(`<li>${message}</li>`));
-  $('#status-messages').show();
-}
-
-const addNewTask = function(event) {
-  event.preventDefault();
-
-  const taskData ={};
-  ['task_name', 'assignee'].forEach( (field) => {
-    const val = $(`input[name=${field}]`).val();
-    if (val != '') {
-      taskData[field] = val;
-    }
-  });
-  const newTask = new Task(taskData);
-
-  if (newTask.isValid()) {
-    taskList.add(newTask);
-    updateStatusMessageWith(`New task added: ${newTask.get('task_name')}`);
-  } else {
-    updateStatusMessageFrom(newTask.validationError);
-  }
-}
-
 $(document).ready( () => {
-  $('#add-task-form').submit(addNewTask);
-
   const taskListView = new TaskListView({
     model: taskList,
     template: _.template($('#task-template').html()),
