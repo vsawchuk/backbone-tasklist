7be234415d176a20c99dda4ded0d8596487c151b
dee <dee@adadevelopersacademy.org>
2017-12-10 00:04:38 -0800

    clean up comments from previous lectures

--- src/views/task_list_view.js
+++ src/views/task_list_view.js
@@ -9,19 +9,14 @@ const TaskListView = Backbone.View.extend({
     this.listenTo(this.model, 'update', this.render);
   },
   render() {
-    // Clear the unordered list
     this.$('#todo-items').empty();
-    // Iterate through the list rendering each Task
     this.model.each((task) => {
-      // Create a new TaskView with the model & template
       const taskView = new TaskView({
         model: task,
         template: this.template,
         tagName: 'li',
         className: 'task',
       });
-      // Then render the TaskView
-      // And append the resulting HTML to the DOM.
       this.$('#todo-items').append(taskView.render().$el);
     });
     return this;
